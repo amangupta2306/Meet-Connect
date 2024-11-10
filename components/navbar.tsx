868a395fe7ab84dad46aa6317b9b24d05fec9e3f
Aman Gupta <guptaaman9036@gmail.com>
2024-11-10 16:07:29 +0530

    minor fixes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,8 +3,8 @@ import { MobileNav } from "./mobile-nav"
 
 export const Navbar =()=>{
     return (
-        <section className="fixed z-50 w-full flex items-center justify-between bg-slate-900 px-5 lg:px-10 py-5">
-            <p className="text-white text-4xl font-semibold">ZOOM</p>
+        <section className="fixed z-50 w-full flex items-center justify-between bg-slate-900 px-5 lg:px-9 py-5">
+            <p className="text-white text-3xl font-semibold">Meet Connect</p>
             <div className="flex items-center gap-5">
                 <SignedIn>
                     <UserButton/>
